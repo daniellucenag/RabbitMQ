bea797823da285e0228d6f2828a07ea5223b9418
Daniel Lucena <daniel.lucena@bs2.com>
2020-04-16 12:44:33 -0300

    Add project files.

---
+++ CaptureCreditCard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CaptureCreditCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
